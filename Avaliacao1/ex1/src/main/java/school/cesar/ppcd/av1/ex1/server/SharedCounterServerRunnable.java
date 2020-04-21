310c2700124475ab2dc68c9e8740c390d745c985
jsantos93 <jcas@cesar.school>
2020-04-21 20:38:52 -0300

    add ex1

--- Avaliacao1/ex1/src/main/java/school/cesar/ppcd/av1/ex1/server/SharedCounterServerRunnable.java
+++ Avaliacao1/ex1/src/main/java/school/cesar/ppcd/av1/ex1/server/SharedCounterServerRunnable.java
@@ -1,41 +1,44 @@
 package school.cesar.ppcd.av1.ex1.server;
 
 import java.net.Socket;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.Semaphore;
 
 public class SharedCounterServerRunnable implements Runnable {
 	private final Socket client;
+	private DataInputStream dataInputStream;
+	private DataOutputStream dataOutputStream;
+	private static final byte[] INC = new byte[] { 'I' };
 	private static Semaphore mutex = new Semaphore(1);
 	private static int valor = 0;
-	public SharedCounterServerRunnable(final Socket client) {
+	public SharedCounterServerRunnable(final Socket client) throws IOException{
 		this.client = client;
+		this.dataInputStream = new DataInputStream(client.getInputStream());
+		this.dataOutputStream = new DataOutputStream(client.getOutputStream());
 	}
 
 	public void run() {
-		while (true) {
-			try {
-				mutex.acquire();
-				try{
-					int valorAtual = valor;
-					valor++;
-					String nome = Thread.currentThread().getName();
-					System.out.println(nome + " : " + valorAtual);
-				} finally{
-					mutex.release();
-				}
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		try {
+			while(true){
+				if(this.dataInputStream.read(INC) == 1){
+					try {
+						mutex.acquire();
+						int valorAtual = valor;
+						valor++;
+						this.dataOutputStream.writeInt(valorAtual);
+						mutex.release();
+						TimeUnit.SECONDS.sleep(1);
 
-			try {
-				TimeUnit.SECONDS.sleep(1);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
 			}
-			
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 
 	}
